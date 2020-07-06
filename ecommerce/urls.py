0ca3be7b4b64e0b3812453f0deeb0e6112a7ba86
DanArmstrong124 <danielarmstrong124@gmail.com>
2020-07-06 23:19:12 +0000

    Updated baseurls

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,10 +15,18 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.views import static
+from search import urls as urls_search
 from accounts import urls as urls_accounts
+from products import urls as urls_products
+from products.views import all_products
+from django.views import static
+from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', all_products, name="index"),
     url(r'^accounts/', include(urls_accounts)),
+    url(r'^products/', include(urls_products)),
+    url(r'^search/', include(urls_search)),
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
 ]
