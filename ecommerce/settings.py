0e9c3361b51b8f640f3f9857099e065a8e8e7f53
DanArmstrong124 <danielarmstrong124@gmail.com>
2020-07-07 13:16:26 +0000

    Updating SETTINGS

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -165,9 +165,6 @@ STATICFILES_DIRS = (
 MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
