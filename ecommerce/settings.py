892f986562b3098d25dbc2d8ab1a6217e348cb7d
DanArmstrong124 <danielarmstrong124@gmail.com>
2020-07-08 00:46:39 +0000

    Attempting to fix image bug

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -143,10 +143,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-AWS_S3_OBJECT_PARAMETERS = {
-    'Expires': 'Fri, 10 Jan 2100 20:00:00 GMT',
-    'CacheControl': 'max-age=999999',
-}
 
 AWS_STORAGE_BUCKET_NAME = 'da-total-graphics'
 AWS_S3_REGION_NAME = 'eu-west-1'
@@ -154,6 +150,11 @@ AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Fri, 10 Jan 2100 20:00:00 GMT',
+    'CacheControl': 'max-age=999999',
+}
 
 STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
