6b99da4d91180ec43127947d20f25499de437712
DanArmstrong124 <danielarmstrong124@gmail.com>
2020-07-09 17:28:35 +0000

    Fixing bugs

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -178,6 +178,6 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
