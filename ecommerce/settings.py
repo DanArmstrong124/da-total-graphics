7154ffe5a418aa59502598ff22fc2eb84c041218
DanArmstrong124 <danielarmstrong124@gmail.com>
2020-07-13 17:20:50 +0000

    Fixing bugs

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -174,3 +174,9 @@ STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
+EMAIL_PORT = 587
